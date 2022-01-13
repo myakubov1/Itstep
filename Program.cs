e99bb1a20b0a0c347e1ecceadd717e83bc1835c0
Якубов Максимилиан Вадимович <myakubov@corp.finam.ru>
2022-01-13 18:57:48 +0300

    Реализован подсчет максимальной последовательности (без индексов максимального промежутка)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace Itstep
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите последовательность: ");
+            int[] binarySequence = Console.ReadLine().ToCharArray().Select(i => int.Parse(i.ToString())).ToArray();  //Кастим введенную последовательность в массив символов
+
+            int endPos = 0;
+            int startPos = 0;
+            int maxLength = 0;
+            int counter = 0;
+            for (int i = 0; i < binarySequence.GetLength(0); i++)
+            {
+                if (binarySequence[i] == 1)
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (counter > maxLength)
+                    {
+                        maxLength = counter;
+                    }
+                    counter = 0;
+                }
+            }
         }
     }
 }
