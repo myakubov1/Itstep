83820a28e21da3f59176107e5c30b2bf42ae5bcd
Якубов Максимилиан <makc1-2-3@list.ru>
2022-01-20 05:34:54 +0300

    Прочитать с клавиатуры массив строк. Перечислить их длины. Вывести на экран первую и последнюю буквы самой длинной строки.

--- Program.cs
+++ Program.cs
@@ -6,41 +6,47 @@ using System.Threading.Tasks;
 
 namespace Itstep
 {
-    //   Задание 5.
-    //Дано целое число N, найти число, полученное при
-    //прочтении числа N справа налево.Например, если было
-    //введено число 345, то программа должна вывести число 543.
+//1. Прочитать с клавиатуры массив строк.
+//Перечислить их длины.Вывести на экран
+//первую и последнюю буквы самой длинной строки.
+
     class Program
     {
         static void Main(string[] args)
         {
-            do
+            int arrayLength;
+
+
+            Console.Write("Количество строк в массиве: ");
+
+            bool isLength = int.TryParse(Console.ReadLine(), out arrayLength); //Проверяем введено ли число в качестве количества элементов массива
+            string[] arrayElements = new string[arrayLength]; //Выделяем место под будущий массив
+
+            for (int i = 0; i < arrayLength; i++)  //Цикл с заполнением массива элементами 
             {
-                int checkInt;
-                string inputString = Console.ReadLine(); //ввод строки в консоль
-                bool isParseble = int.TryParse(inputString, out checkInt); //проверка на то, является ли введеная строка натуральным числом
+                Console.Write("Введите строку {0}: ", i + 1);
+                arrayElements[i] = Console.ReadLine();
+            }
 
-                if (isParseble == true)
+            for (int i = 0; i < arrayLength; i++) //Цикл для вывода элементов массива в консоль (для удобства)
+            {
+                Console.WriteLine($"Элемент №{i}: {arrayElements[i]} | Длина эелмента: {arrayElements[i].Length}");
+            }
+
+
+            int maxLength = arrayElements[0].Length;
+            string maxElement = arrayElements[0];
+            foreach (string s in arrayElements)
+            {
+                if (s.Length > maxLength)
                 {
-                    //Создаем два массива.
-                    //Второму массиву присваиваем значения первого (итерация в обрватном порядке).
-                    char[] beforeReverse = inputString.ToCharArray(); 
-                    char[] afterReverse = new char[beforeReverse.Length];
-                    int count = 0;
-
-                    for (int i = beforeReverse.Length - 1; i >= 0; i--)
-                    {
-                        afterReverse[count] = beforeReverse[i];
-                        count++;
-                    }
-                    Console.WriteLine(afterReverse);
+                    maxLength = s.Length;
+                    maxElement = s;
                 }
-                else  //если не является натуральным числом
-                {
-                    Console.WriteLine("Введенная строка не является натуральным числом");
-                }
-
-            } while (true);
+            }
+            Console.WriteLine($"Размер самой длинной строки: {maxLength} | Строка: {maxElement}");
+            char[] charArrayMaxElement = maxElement.ToCharArray();
+            Console.WriteLine($"Первый символ: {charArrayMaxElement[0]} | Последний символ: {charArrayMaxElement[charArrayMaxElement.Length - 1]}");
         }
     }
 }
