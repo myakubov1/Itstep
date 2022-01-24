1190a2357ae5bbb9f755c007e8e134b7aaee381e
Якубов Максимилиан <makc1-2-3@list.ru>
2022-01-25 01:38:05 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -6,41 +6,149 @@ using System.Threading.Tasks;
 
 namespace Itstep
 {
-//4. Создать массив. Пройти по нему циклом.
-//Для каждого числа создать строку из этих цифр этой длины.
-
 
     class Program
     {
         static void Main(string[] args)
         {
+            double[] A = new double[5];
+            double[,] B = new double[3, 4];
+            double minRange = -10.0;
+            double maxRange = 10.0;
             Random random = new Random();
-            Random random1 = new Random();
 
-            int[] arrayElements = new int[random1.Next(1,10)]; 
-
-            for (int i = 0; i < arrayElements.Length; i++)  //Заполнение массива
+            for (int i = 0; i < A.Length; i++)
             {
-                arrayElements[i] = random.Next(1, 9);
-
+                Console.Write("Введите число: ");
+                A[i] = double.Parse(Console.ReadLine());
             }
-            int k = 0;
-            for (int i = 0; i < arrayElements.Length; i++)  
+
+            for (int i = 0; i < B.GetLength(0); i++)
             {
-                do
+                for (int q = 0; q < B.GetLength(1); q++)
                 {
-                    Console.Write(arrayElements[i]);
-                    k++;
-                    
+                    B[i, q] = random.NextDouble() * (maxRange - minRange) + minRange;
+
                 }
-                while (k<arrayElements[i]);
-                k = 0;
-                Console.WriteLine("");
             }
 
-           
-            
+            /*==========================================================================================================================================  */
 
+            Console.WriteLine($"Одномерный массив: {string.Join(" ", A)}");
+
+            for (int i = 0; i < B.GetLength(0); i++, Console.WriteLine())
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    Console.Write("{0,3} ", B[i, j]);
+                }
+            }
+
+            /*==========================================================================================================================================  */
+
+      
+
+            List<double> genValues = new List<double>();
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int q = 0; q < B.GetLength(0); q++)
+                {
+                    for (int j = 0; j < B.GetLength(1); j++)
+                    {
+                        if (A[i] == Math.Round(B[q, j]))
+                        {
+                            genValues.Add(A[i]);
+                        }
+                    }
+                }
+
+            }
+
+            /*==========================================================================================================================================  */
+
+            if (genValues.Count > 0)
+            {
+                double minValue = genValues[0];
+                double maxValue = genValues[0];
+                for (int z = 0; z < genValues.Count; z++)
+                {
+                    if (genValues[z] > maxValue)
+                    {
+                        maxValue = genValues[z];
+                    }
+
+                    if (genValues[z] < minValue)
+                    {
+                        minValue = genValues[z];
+                    }
+                }
+                Console.WriteLine($"Максимальное общее значение (с округлением): {maxValue}");
+                Console.WriteLine($"Минимальное общее значение (с округлением): {minValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Общих элементов не найдено");
+            }
+
+            /*==========================================================================================================================================  */
+
+            double summValue = 0;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                summValue += A[i];
+            }
+
+            for (int q = 0; q < B.GetLength(0); q++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    summValue += B[q, j];
+                }
+            }
+            Console.WriteLine($"Сумма значений всех массивов: {summValue}");
+
+            /*==========================================================================================================================================  */
+
+            double multiValue = 1;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                multiValue *= A[i];
+            }
+            for (int q = 0; q < B.GetLength(0); q++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    multiValue *= B[q, j];
+                }
+            }
+            Console.WriteLine($"Произведение значений всех массивов: {multiValue}");
+
+            /*==========================================================================================================================================  */
+
+            double summEvenA=0;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+               if (A[i]%2 ==0)
+                {
+                    summEvenA += A[i];
+                }
+            }
+            Console.WriteLine($"Сумма четных элементов массива A: {summEvenA}");
+
+            /*==========================================================================================================================================  */
+
+            double summOddColB=0;
+            for (int q = 0; q < B.GetLength(0); q++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    if (j%2 !=0)
+                    {
+                        summOddColB += B[q, j];
+                    }
+                }
+            }
+            Console.WriteLine($"Сумма нечетных столбцов массива B: {summOddColB}");
         }
     }
 }
