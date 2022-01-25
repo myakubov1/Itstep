4ad610fb599920973a5fa26a389cbe87e6a644e2
Якубов Максимилиан <makc1-2-3@list.ru>
2022-01-25 21:18:32 +0300

    Reference and value types

--- Program.cs
+++ Program.cs
@@ -11,144 +11,49 @@ namespace Itstep
     {
         static void Main(string[] args)
         {
-            double[] A = new double[5];
-            double[,] B = new double[3, 4];
-            double minRange = -10.0;
-            double maxRange = 10.0;
-            Random random = new Random();
+            student student;   
+            student.name = "Макс";
+            student.age = 18;
 
-            for (int i = 0; i < A.Length; i++)
-            {
-                Console.Write("Введите число: ");
-                A[i] = double.Parse(Console.ReadLine());
-            }
+            student student2 = new student("Коля", 20);  
+            student = student2;
+            student2.name = "Андрей";
+            student2.age = 19;
 
-            for (int i = 0; i < B.GetLength(0); i++)
-            {
-                for (int q = 0; q < B.GetLength(1); q++)
-                {
-                    B[i, q] = random.NextDouble() * (maxRange - minRange) + minRange;
 
-                }
-            }
+            /* ============================================= */
 
-            /*==========================================================================================================================================  */
+            Stud stud = new Stud();
+            stud.name = "Макс";
+            stud.age = 18;
 
-            Console.WriteLine($"Одномерный массив: {string.Join(" ", A)}");
+            Stud stud2 = new Stud();
+            stud = stud2;
+            stud2.name = "Андрей";
+            stud2.age = 19;
 
-            for (int i = 0; i < B.GetLength(0); i++, Console.WriteLine())
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    Console.Write("{0,3} ", B[i, j]);
-                }
-            }
-
-            /*==========================================================================================================================================  */
-
-      
-
-            List<double> genValues = new List<double>();
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-                for (int q = 0; q < B.GetLength(0); q++)
-                {
-                    for (int j = 0; j < B.GetLength(1); j++)
-                    {
-                        if (A[i] == Math.Round(B[q, j]))
-                        {
-                            genValues.Add(A[i]);
-                        }
-                    }
-                }
-
-            }
-
-            /*==========================================================================================================================================  */
-
-            if (genValues.Count > 0)
-            {
-                double minValue = genValues[0];
-                double maxValue = genValues[0];
-                for (int z = 0; z < genValues.Count; z++)
-                {
-                    if (genValues[z] > maxValue)
-                    {
-                        maxValue = genValues[z];
-                    }
-
-                    if (genValues[z] < minValue)
-                    {
-                        minValue = genValues[z];
-                    }
-                }
-                Console.WriteLine($"Максимальное общее значение (с округлением): {maxValue}");
-                Console.WriteLine($"Минимальное общее значение (с округлением): {minValue}");
-            }
-            else
-            {
-                Console.WriteLine($"Общих элементов не найдено");
-            }
-
-            /*==========================================================================================================================================  */
-
-            double summValue = 0;
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-                summValue += A[i];
-            }
-
-            for (int q = 0; q < B.GetLength(0); q++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    summValue += B[q, j];
-                }
-            }
-            Console.WriteLine($"Сумма значений всех массивов: {summValue}");
-
-            /*==========================================================================================================================================  */
-
-            double multiValue = 1;
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-                multiValue *= A[i];
-            }
-            for (int q = 0; q < B.GetLength(0); q++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    multiValue *= B[q, j];
-                }
-            }
-            Console.WriteLine($"Произведение значений всех массивов: {multiValue}");
-
-            /*==========================================================================================================================================  */
-
-            double summEvenA=0;
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-               if (A[i]%2 ==0)
-                {
-                    summEvenA += A[i];
-                }
-            }
-            Console.WriteLine($"Сумма четных элементов массива A: {summEvenA}");
-
-            /*==========================================================================================================================================  */
-
-            double summOddColB=0;
-            for (int q = 0; q < B.GetLength(0); q++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    if (j%2 !=0)
-                    {
-                        summOddColB += B[q, j];
-                    }
-                }
-            }
-            Console.WriteLine($"Сумма нечетных столбцов массива B: {summOddColB}");
         }
     }
+
+
+    struct student
+    {
+        public string name;   
+        public int age;     
+        
+        public student(string Name, int Age)  
+        {
+            name = Name;
+            age = Age;   
+        }
+    }
+
+    class Stud
+    {
+        public string name;
+        public byte age;
+
+       
+    }
 }
+
