1fd829ffcf69d4db24e7d949bd1997adb4837bee
Якубов Максимилиан <makc1-2-3@list.ru>
2022-01-26 07:00:59 +0300

    Задание 7 Сделано с помощью регулярных выражений

--- Program.cs
+++ Program.cs
@@ -2,99 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Itstep
 {
-    //Сложение матриц;
     class Program
     {
+        //        Создайте приложение, проверяющее текст на недопустимые слова.Если недопустимое слово найдено, оно
+        //должно быть заменено на набор символов*. По итогам
+        //работы приложения необходимо показать статистику
+        //действий. 
         static void Main(string[] args)
         {
 
-            Console.Write("Количество строк матрицы A: ");
-            int r = int.Parse(Console.ReadLine());
-            Console.Write("Количество столбцов матрицы A: ");
-            int c = int.Parse(Console.ReadLine());
+            string[] badWords = new string[] { "be", "is", "die" };
+            string baseText = "test, and bee be not testing.is. is  is,  But yes to test";
+            string regPattern;
+            string newValue;
+            int counter = 0;
 
-            int[,] A = new int[r, c];
 
-            for (int i = 0; i < r; i++)
+            for (int i = 0; i < badWords.Length; i++)
             {
-                for (int j = 0; j < c; j++)
+                Regex regPatter = new Regex(@"\b" + badWords[i] + @"\b");
+                MatchCollection matches = regPatter.Matches(baseText);
+
+                if (baseText.IndexOf(badWords[i]) > 0)
                 {
-                    Console.Write("[{0}][{1}] = ", i, j);
-                    A[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
 
-
-
-            Console.Write("Количество строк матрицы B: ");
-            int r2 = int.Parse(Console.ReadLine());
-            Console.Write("Количество столбцов матрицы B: ");
-            int c2 = int.Parse(Console.ReadLine());
-
-            int[,] B = new int[r2, c2];
-
-            for (int i = 0; i < r2; i++)
-            {
-                for (int j = 0; j < c2; j++)
-                {
-                    Console.Write("[{0}][{1}] = ", i, j);
-                    B[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("-----------------------------------------------------");
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-                for (int j = 0; j < A.GetLength(1); j++)
-                {
-                    Console.Write(A[i, j].ToString().PadLeft(5));
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("-----------------------------------------------------");
-
-            for (int i = 0; i < B.GetLength(0); i++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    Console.Write(B[i, j].ToString().PadLeft(5));
-                }
-                Console.WriteLine();
-            }
-
-            if (A.GetLength(0) != B.GetLength(0) || A.GetLength(1) != B.GetLength(1))
-            {
-                Console.WriteLine("Количество столбцов и строк A должно быть равно количеству стольбцов и строк  B!");
-                return;
-            }
-            else
-            {
-                int[,] C = new int[A.GetLength(0), B.GetLength(1)];
-
-
-                for (int i = 0; i < A.GetLength(0); i++)
-                {
-                    for (int j = 0; j < B.GetLength(1); j++)
-                    {
-                        C[i, j] = A[i, j] + B[i, j];
-                    }
+                    regPattern = @"\b" + badWords[i] + @"\b";
+                    newValue = string.Concat(Enumerable.Repeat("*", badWords[i].Length));
+                    baseText = Regex.Replace(baseText, regPattern, newValue, RegexOptions.IgnoreCase);
+                    counter += matches.Count;
                 }
 
-                Console.WriteLine("-----------------------------------------------------");
-                Console.WriteLine("Итоговая матрица");
-                for (int i = 0; i < C.GetLength(0); i++)
-                {
-                    for (int j = 0; j < C.GetLength(1); j++)
-                    {
-                        Console.Write(C[i, j].ToString().PadLeft(5));
-                    }
-                    Console.WriteLine();
-                }
 
             }
+            Console.WriteLine(baseText);
+            Console.WriteLine($"Число замен: {counter}");
         }
 
     }
