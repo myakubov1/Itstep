bf3a8c68b2cf5453793f2ac0a1ea963b96bf9d2b
Якубов Максимилиан <makc1-2-3@list.ru>
2022-01-31 15:54:44 +0300

    Задание 1 Создайте приложение калькулятор для перевода числа из одной системы исчисления в другую. Пользователь с помощью меню выбирает направление перевода. Например, из десятичной в двоичную. После выбора направления, пользователь вводит число в исходной системе исчисления. Приложение должно перевести число в требуемую систему. Предусмотреть случай выхода за границы диапазона, определяемого типом int, неправильный ввод.

---
+++ M3C2/Ex1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itstep.M3C2
+{
+    class Ex1
+    {
+        public static double BinToDec(string binNumber)
+        {
+            int power = 0;
+            double result = 0;
+            try
+            {
+                for (var i = binNumber.Length - 1; i >= 0; i--)
+                {
+                    if (binNumber[i] == '1')
+                    {
+                       checked( result) = result + Convert.ToUInt32(Math.Pow(2, power));
+                    }
+                    power++;
+                }
+                return  result;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        public static string DecToBin(int decNumber)
+        {
+            var binNumber = string.Empty;
+            while (decNumber > 0)
+            {
+                binNumber = (decNumber % 2) + binNumber;
+                decNumber = decNumber / 2;
+            }
+
+            return binNumber;
+        }
+
+        public static void Exercise1()
+        {
+
+
+
+
+            Console.WriteLine("Из десятичной в двоичную : 1");
+            Console.WriteLine("Из двоичной в десятичную: 2");
+            string choise = Console.ReadLine();
+
+            Console.Write("Введите число: ");
+            string Num = Console.ReadLine();
+            switch (choise)
+            {
+                case "1":
+                    Console.WriteLine("В двоичной: {0}", DecToBin(int.Parse(Num)));
+                    break;
+                case "2":
+                    Console.WriteLine("В десятичной: {0}", BinToDec(Num));
+                    break;
+                default:
+                    Console.WriteLine("Нету такого перевода");
+                    break;
+            }
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,43 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Itstep.M3C2;
 
 namespace Itstep
 {
-//  Задание 6
-//Пользователь с клавиатуры вводит некоторый текст.
-//Приложение должно изменять регистр первой буквы
-//каждого предложения на букву в верхнем регистре.
-//ДОМАШНЕЕ ЗАДАНИЕ
-//2
-//Например, если пользователь ввёл: «today is a good
-//day for walking.i will try to walk near the sea».
-//Результат работы приложения: «Today is a good day
-//for walking.I will try to walk near the sea».
     class Program
     {
 
         static void Main(string[] args)
         {
-            Console.Write("Текст: ");
-            string myText = Console.ReadLine();
-            myText = "." + myText;
-
-            string[] arrayOfSentences = myText.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-
-            for (int i = 0; i < arrayOfSentences.Length; i++)
-            {
-                arrayOfSentences[i] = arrayOfSentences[i].Trim();
-                char[] ch = arrayOfSentences[i].ToCharArray();
-                ch[0] = char.ToUpper(ch[0]);
-                arrayOfSentences[i] = string.Join("", ch);
-            }
-            string outputText = string.Join(". ", arrayOfSentences);
-            Console.WriteLine(outputText);
-
+            Ex1.Exercise1();
         }
-    }
 
+        
+
+    }
 }
 
 
