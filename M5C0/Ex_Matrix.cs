460fe2cdb07c0395acdc3ffe6ab08af0c9c3c05e
Якубов Максимилиан <makc1-2-3@list.ru>
2022-02-05 23:38:48 +0300

    Задание с матрциами и классами

---
+++ M5C0/Ex_Matrix.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itstep.M5C0
+{
+
+
+
+    class Matrix
+    {
+        int Rows { get; set; }
+        int Columns { get; set; }
+        int[,] MatrixValues { get; set; }
+
+        public Matrix(int countRows, int countColumns)
+        {
+            this.Rows = countRows;
+            this.Columns = countColumns;
+            MatrixValues = new int[countRows, countColumns];
+
+        }
+
+        public static void ShowExercise()
+        {
+            Console.WriteLine("Введите количество строк: ");
+            int r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Matrix A = new Matrix(r, c);
+            A.Create();
+            A.Print();
+
+            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+            Matrix B = new Matrix(r, c);
+            B.Create();
+            B.Print();
+
+            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+            Matrix C = A + B;
+            C.Print();
+
+            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+            C = A - B;
+            C.Print();
+
+            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+
+            C = A * B;
+            C.Print();
+        }
+
+        public void Create()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    MatrixValues[i, j] = rnd.Next(1000000, 1000000) + rnd.Next(1000000, 1000000);
+                }
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write(MatrixValues[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matrix operator +(Matrix A, Matrix B)
+        {
+            Matrix C = new Matrix(A.Rows, B.Columns);
+
+
+            if ((A.Columns != B.Columns) || (A.Rows != B.Rows))
+            {
+                throw new Exception("Для матриц с разным размером сложение не возможно!");
+            }
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < B.Columns; j++)
+                {
+                    C.MatrixValues[i, j] = A.MatrixValues[i, j] + B.MatrixValues[i, j];
+                }
+            }
+
+            return C;
+        }
+
+
+        public static Matrix operator -(Matrix A, Matrix B)
+        {
+            Matrix C = new Matrix(A.Rows, B.Columns);
+
+
+            if ((A.Columns != B.Columns) || (A.Rows != B.Rows))
+            {
+                throw new Exception("Для матриц с разным размером вычитание не возможно!");
+            }
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < B.Columns; j++)
+                {
+                    C.MatrixValues[i, j] = A.MatrixValues[i, j] - B.MatrixValues[i, j];
+                }
+            }
+
+            return C;
+        }
+
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            Matrix C = new Matrix(A.Rows, B.Columns);
+
+
+            if (A.Columns != B.Rows)
+            {
+                throw new Exception("Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+            }
+
+            for (var i = 0; i < A.Rows; i++)
+            {
+                for (var j = 0; j < B.Columns; j++)
+                {
+                    C.MatrixValues[i, j] = 0;
+                    try
+                    {
+                        for (int k = 0; k < A.Columns; k++)
+                        {
+                            checked
+                            {
+                                C.MatrixValues[i, j] += A.MatrixValues[i, k] * B.MatrixValues[k, j];
+                            }
+                        }
+                    }
+                    catch (OverflowException ex)
+                    {
+                        throw new Exception($"{ex.Message} в матрице С [{i}] [{j}]");
+                    }
+                }
+            }
+
+            return C;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Itstep.M3C2;
+using Itstep.M5C0;
 
 namespace Itstep
 {
@@ -16,40 +17,13 @@ namespace Itstep
             //Ex1.Exercise1();
 
             //Ex3.Exercise3();
-            Book book = new Book("Book1", "M.Yakubov", "ItStep", "Edit#3", DateTime.Now, 600);
-            Book book2 = new Book("Book2", "M.Yakubov2", "ItStep2", "Edit#4", DateTime.Now, 700);
-            Console.WriteLine(book.Author);
-            Library library1 = new Library() { Name = "Lib1", Address = "Kutuz", Book = book, Phone = "88005553535" };
 
-        }
+            Matrix.ShowExercise();
 
-        class Library
-        {
-            public string Name { get; set; }
-            public string Address { get; set; }
-            public string Phone { get; set; }
-            public Book Book { get; set; }
-        }
 
-        class Book
-        {
-            public Book(string name, string author, string publisher, string edition, DateTime year, int pageCount)
-            {
-                Name = name;
-                Author = author;
-                Publisher = publisher;
-                Edition = edition;
-                Year = year;
-                PageCount = pageCount;
-            }
-            public string Name { get; }
-            public string Author { get; }
-            public string Publisher { get; }
-            public string Edition { get; }
-            public DateTime Year { get; }
-            public int PageCount { get; }
+
+
         }
     }
+
 }
-
-
